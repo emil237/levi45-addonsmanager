e572959dc11bdcd25f0d58aaf969961f1b3898d7
emil237 <95539537+emil237@users.noreply.github.com>
2022-04-24 09:49:10 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -8,10 +8,10 @@
 echo "***********************************************************************************************************************"
 # Config script #
 TMPDIR='/tmp'
-VERSION='9.5'
+VERSION='3.4'
 MY_URL='https://raw.githubusercontent.com/emil237/levi45-addonsmanager/main'
-MY_IPK="enigma2-plugin-extensions-levi45multicammanager_9.5_all.ipk"
-MY_DEB="enigma2-plugin-extensions-levi45multicammanager_9.5_all.deb"
+MY_IPK="enigma2-plugin-extensions-levi45-addonsmanager_3.4_all.ipk"
+MY_DEB="enigma2-plugin-extensions-levi45-addonsmanager_3.4_all.deb"
 ####################
 MY_EM='============================================================================================================'
 #  Remove Old Plugin  #
@@ -59,6 +59,8 @@ wait
 init 4
 init 3
 exit 0
+
+
 
 
 
